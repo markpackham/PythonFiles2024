e99d09578392bc5980f0e54122d2c680164e0e01
mark <markpackham1@gmail.com>
2024-08-26 12:41:48 +0100

    Fix path bug, use parent directory name
    https://youtu.be/TZkIud0u2C4?si=UJv8sHwvkQ4WIkjD&t=5

--- Working with Paths/app.py
+++ Working with Paths/app.py
@@ -5,8 +5,9 @@ file = open('characters.txt', 'r')
 
 def create_path():
     # Construct new path object
-    # path gets determined at runtime
-    script_dir = Path(__file__)
+    # absolute path gets determined at runtime
+    # Make sure ".parent" is added at end or it will be confused by "characters" as a file and as a directory
+    script_dir = Path(__file__).parent
     # print(script_dir.parent)
 
     # Add a folder to this path (won't exist yet)
