3c130f8a17dcc2720bafdade61451a0fd3a320b4
mark <markpackham1@gmail.com>
2024-08-26 13:09:03 +0100

    Create file in specified path
    https://youtu.be/TZkIud0u2C4?si=waI_aeeayccURXOl&t=169

---
+++ Pathlib Read & Write Files/app.py
@@ -0,0 +1,30 @@
+# pathlib module (easier than using the "os" module although both are built into python)
+from pathlib import Path
+
+file = open('characters.txt', 'r')
+
+def create_path():
+    script_dir = Path(__file__).parent
+    # print(script_dir.parent)
+
+    # Add a folder to this path (won't exist yet)
+    path = script_dir / 'characters'
+    
+    # Make the folder exist
+    path.mkdir(parents=True, exist_ok=True)
+
+    # Add absolute path to file which doesn't yet exist
+    path = path / 'zelda.txt'
+
+    # Use open method on "path" so it will already know of the path's existence
+    # so we can avoid having to put path in as an argument
+    file = path.open('w')
+    file.write("Ganon")
+
+    return
+
+def main():
+    create_path()
+
+if __name__ == "__main__":
+    main()
