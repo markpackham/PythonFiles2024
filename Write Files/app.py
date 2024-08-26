6ef134c1bca343e2774f164c96281096fdc376b9
mark <markpackham1@gmail.com>
2024-08-26 12:03:22 +0100

    Demo write then read (use seek() to reset reading pointer)
    https://www.youtube.com/watch?v=z1Klw2k_KJk&list=PL4cUxeGkcC9inv02BGhWLGFp2Zyktg8Dk&index=3

--- Write Files/app.py
+++ Write Files/app.py
@@ -4,11 +4,20 @@ characters = ["Mario", "Lugi", "Peach", "Yoshi", "Bowser", "Toad"]
 
 def write_characters_to_file(filename):
     # Open file
-    file = open(filename, 'w')
+    # file = open(filename, 'w')
 
-    # Write characters to file
+    # with w+ we can Read & Write
+    file = open(filename, 'w+')
+
+    # Write characters to file with new line
     for c in characters:
-        file.write(c)
+        file.write(c + "\n")
+
+    # Reset pointer back to the start via see() or we'd read the end of the file with nothing
+    file.seek(0,0)
+
+    content = file.read()
+    print(content)
 
     # Close file
     file.close()
