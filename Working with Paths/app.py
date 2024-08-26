490647b1019fbe144ee5fa373264ccd99164443b
mark <markpackham1@gmail.com>
2024-08-26 12:35:47 +0100

    Attempt to create new folder
    https://youtu.be/p0Cy0LQ-sLw?si=JgNIf-MyEZD58iDG&t=510

--- Working with Paths/app.py
+++ Working with Paths/app.py
@@ -1,8 +1,20 @@
-# Pathlib files
+# pathlib module (easier than using the "os" module although both are built into python)
+from pathlib import Path
 
 file = open('characters.txt', 'r')
 
 def create_path():
+    # Construct new path object
+    # path gets determined at runtime
+    script_dir = Path(__file__)
+    # print(script_dir.parent)
+
+    # Add a folder to this path (won't exist yet)
+    path = script_dir / 'characters'
+    
+    # Make the folder exist
+    path.mkdir(parents=True, exist_ok=True)
+
     return
 
 def main():
