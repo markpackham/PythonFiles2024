10fcbeb3e8b0e61d5340aad34e68fe10a6fa9df3
mark <markpackham1@gmail.com>
2024-08-26 11:49:05 +0100

    Print file contents in separate lines
    https://youtu.be/0mxeoYsXTMg?si=nyMoWoW9LpMLb2Mk&t=380

--- Opening & Reading Files/app.py
+++ Opening & Reading Files/app.py
@@ -1,9 +1,25 @@
 # Reading files
 
 def read_file():
+    # Open file ("open()" path is relative to your position in the terminal not the app.py location itself)
+    file = open('characters.txt', 'r')
+
+    # Read file
+    # content = file.read()
+    # print(content)
+
+    lines = file.readlines()
+
+    for line in lines:
+        print(line)
+
+    # Close file
+    file.close()
+
     return
 
 def main():
+    read_file()
     return
 
 if __name__ == "__main__":
